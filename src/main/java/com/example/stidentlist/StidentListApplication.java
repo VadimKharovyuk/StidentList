edaf45d178c13a489cda9d9212df8b8c87307ae7
VadimKharovyuk <146035315+VadimKharovyuk@users.noreply.github.com>
2024-02-12 04:52:04 +0530

    add table  2 user

--- src/main/java/com/example/stidentlist/StidentListApplication.java
+++ src/main/java/com/example/stidentlist/StidentListApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StidentListApplication  {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(StidentListApplication.class, args);
 	}
